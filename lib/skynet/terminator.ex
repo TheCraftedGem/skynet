b1e65cf5f0500f8e600c29e72d93abc4cf5efc3c
Emilio <thecraftedgem@tuta.io>
2019-04-28 15:27:04 -0600

    refactoring

--- lib/skynet/terminator.ex
+++ lib/skynet/terminator.ex
@@ -1,10 +1,7 @@
 require IEx
 
 defmodule Skynet.Terminator do
-  alias Skynet.TerminatorTracker
   use GenServer
-
-
 
   def start_link(_) do
     # Creates GenServer Process
@@ -20,49 +17,46 @@ defmodule Skynet.Terminator do
 
 
   def handle_info(:prepare_next, state) do
-    if reproduce? == true do
+    if reproduce?() == true do
       IO.puts("Terminator Created")
-      #Calls The Parent Module
       Skynet.create_terminator
     else
-      IO.puts("Termiated Failed To Create")
+      IO.puts("Terminator Failed To Create")
       prepare_next()
     end
     {:noreply, state}
   end
 
   def handle_info(:battles_sarah, state) do
-    # Do the desired work here
-    if killed? == true do
+    if killed?() == true do
       IO.puts("Terminator Killed!")
-      Process.exit(self, :kill)
+      Skynet.kill_terminator(self())
     else
-      IO.puts("Termiated Survives")
-      prepare_next()
+      IO.puts("Terminator Survives")
+      battles_sarah()
+      {:noreply, state}
     end
-    {:noreply, state}
   end
 
   defp prepare_next do
     # Waits 5 Seconds To Perform Action
-    Process.send_after(self(), :prepare_next, 5 * 1000)
+    Process.send_after(self(), :prepare_next, 5000)
   end
 
   defp battles_sarah do
     # Waits 10 Seconds To Perfome Action
-    Process.send_after(self(), :battles_sarah, 10 * 1000)
+    Process.send_after(self(), :battles_sarah, 10000)
   end
 
   def killed? do
     # Every 10 Seconds 25% chance of getting killed by sarah
-    Enum.random([false, false, false, true])
+    :rand.uniform <= 0.25
     #  If True kill_current_agent and print terminator killed
   end
 
   def reproduce? do
     #  Every 5 Seconds 20% Chanace Reproducing
-    Enum.random([false, false, false, false, true])
+    :rand.uniform <= 0.20
     # If true create new terminator and print terminator spawned
   end
-
 end
